83c5a459b6db5ec2bb663eef5bc7d7e96404bef4
Robetjunior <juniorferreirarob@gmail.com>
2025-03-19 17:27:01 -0300

    feat: add API service (api.js) with axios configuration and token interceptor

---
+++ sps-web/src/services/api.js
@@ -0,0 +1,20 @@
+// src/services/api.js
+import axios from 'axios';
+
+const api = axios.create({
+  baseURL: 'http://localhost:3000',
+});
+
+// Interceptor para adicionar o token no header de todas as requisições
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      config.headers['Authorization'] = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+
+export default api;
